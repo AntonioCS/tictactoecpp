1279c301c06a101922c27bfc8b345201d994a697
AntonioCS <antoniocs@gmail.com>
2018-02-12 17:15:36 +0000

    Renderer

--- src/AcsGameEngine/Renderer.cpp
+++ src/AcsGameEngine/Renderer.cpp
@@ -1,19 +1,39 @@
 #include "Renderer.h"
-/*
+#include "GameWindow.h"
+#include <iostream>
+
 namespace AcsGameEngine {
 
-    Renderer::Renderer(std::unique_ptr<GameWindow> window) : m_window(window) {
-        m_renderer = SDL_CreateRenderer(m_window->getWindow(), -1, SDL_RENDERER_ACCELERATED);
+    Renderer::Renderer(const GameWindow &window, int index, Uint32 flags) : m_window(window) {
+        SDL_Window *w = m_window.getWindow();
+        m_renderer = SDL_CreateRenderer(w, index, flags);
         if (m_renderer == nullptr) {
             throw std::runtime_error(std::string{"Unable to initialize renderer: "}
             +std::string{SDL_GetError()});
         }
     }
 
-    Renderer::Renderer(const Renderer& orig) {
-    }
 
     Renderer::~Renderer() {
     }
+
+    Renderer &Renderer::DrawColor(Util::ColorList::Color &color, uint8_t alpha) {
+        DrawColor(color.r, color.g, color.b, alpha);
+        return *this;
+    }
+
+    Renderer &Renderer::DrawColor(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha) {
+        SDL_SetRenderDrawColor(m_renderer, r, g, b, alpha);
+        return *this;
+    }
+
+    Renderer &Renderer::Clear() {
+        SDL_RenderClear(m_renderer);
+        return *this;
+    }
+
+    Renderer &Renderer::Present() {
+        SDL_RenderPresent(m_renderer);
+        return *this;
+    }
 }
-*/
--- src/AcsGameEngine/Renderer.h
+++ src/AcsGameEngine/Renderer.h
@@ -2,22 +2,28 @@
 #define RENDERER_H
 
 #include <SDL2/SDL.h>
-#include <memory>
-/*
+#include "Util/ColorList.h"
+
 namespace AcsGameEngine {
+
 
     class GameWindow;
 
     class Renderer {
     public:
-        Renderer(std::unique_ptr<GameWindow> window);
-        Renderer(const Renderer& orig);
+        Renderer(const GameWindow &window, int index = -1, Uint32 flags = SDL_RENDERER_ACCELERATED);
+        Renderer(const Renderer& orig) = delete;
         virtual ~Renderer();
+
+        Renderer &DrawColor(Util::ColorList::Color &color, uint8_t alpha = 255);
+        Renderer &DrawColor(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha = 255);
+        Renderer &Clear();
+        Renderer &Present();
     private:
-        SDL_Renderer *m_renderer = nullptr;        
-        std::unique_ptr<GameWindow> m_window;
+        SDL_Renderer *m_renderer = nullptr;
+        const GameWindow &m_window;
     };
 }
-*/
+
 #endif /* RENDERER_H */
 
