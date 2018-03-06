cc9d4388f00abcc914ff8fe4ef405009e7da0f2e
AntonioCS <antoniocs@gmail.com>
2018-03-06 20:04:26 +0000

    Moving to a more c++17 code

--- src/AcsGameEngine/Renderer.h
+++ src/AcsGameEngine/Renderer.h
@@ -2,26 +2,31 @@
 #define RENDERER_H
 
 #include <SDL2/SDL.h>
+#include <memory>
 #include "Util/ColorList.h"
 
 namespace AcsGameEngine {
 
 
-    class GameWindow;
+    class Window;
 
     class Renderer {
     public:
-        Renderer(const GameWindow &window, int index = -1, Uint32 flags = SDL_RENDERER_ACCELERATED);
+        Renderer(const Window &window, int index = -1, Uint32 flags = SDL_RENDERER_ACCELERATED);
         Renderer(const Renderer& orig) = delete;
-        virtual ~Renderer();
 
-        Renderer &DrawColor(Util::ColorList::Color &color, uint8_t alpha = 255);
+        Renderer &DrawColor(const Util::ColorList::Color &, uint8_t alpha = 255);
         Renderer &DrawColor(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha = 255);
         Renderer &Clear();
+        Renderer &Clear(const Util::ColorList::Color &);
         Renderer &Present();
+
+        SDL_Renderer *getRawPointer() const;
+        const Window &getWindow() const;
     private:
-        SDL_Renderer *m_renderer = nullptr;
-        const GameWindow &m_window;
+        std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer) > m_renderer;
+        const Window &m_window;
+        SDL_Renderer *createRendererPointer(SDL_Window *w, int index, Uint32 flags);
     };
 }
 
